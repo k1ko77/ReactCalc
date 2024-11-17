7d9e7cc61921ad408a06c01ceb925c02ee25cdb5
FlashAnton <safronov-ai@mail.ru>
2024-11-17 22:03:23 +0300

    Обновлены комментарии

--- src/App.js
+++ src/App.js
@@ -1,99 +1,100 @@
-// Вытащили математику
+// В сценарии используется
+// виток "применить состояние"
 import { useState } from "react";
 
 function App() {
-  const [calc, setCalc] = useState("");
-  const [result, setResult] = useState("");
+	const [calc, setCalc] = useState("");
+	const [result, setResult] = useState("");
 
-  const ops =['/', '*', '+', '-', '.'];
+	const ops =['/', '*', '+', '-', '.'];
 
-  const updateCalc = value => {
-    // Заперетили спользовать операторы больше одного раза и вводить ранше чисел
-    if (
-      ops.includes(value) && calc === '' ||
-      ops.includes(value) && ops.includes(calc.slice(-1))
-    ) {
-      return;
-    }
+    // Ввод заперета на использование операций 
+	// больше одного раза и на использование
+	// их раньше, чем ввод чисел
+	const updateCalc = value => {
+		if (
+			ops.includes(value) && calc === '' ||
+			ops.includes(value) && ops.includes(calc.slice(-1))
+		) {
+			return;
+		}
 
-    setCalc(calc + value);
+		setCalc(calc + value);
 
-    if (!ops.includes(value)) {
-      setResult(eval(calc + value).toString());
-    }
-  }
-  // функция для создания цифр от 1 до 10
-  const createDigits = () => {
-    const digits = [];
+		if (!ops.includes(value)) {
+			setResult(eval(calc + value).toString());
+		}
+	}
+  
+	// Функция для создания цифр от 1 до 9
+	const createDigits = () => {
+		const digits = [];
 
-    for (let i = 1; i < 10; i++) {
-      digits.push(
-        <button 
-          onClick={() => updateCalc(i.toString())}
-          key={i}>
-          {i}
-        </button>
-      )
-    }
+		for (let i = 1; i < 10; i++) {
+			digits.push(
+				<button 
+					onClick={() => updateCalc(i.toString())}
+					key={i}>
+					{i}
+				</button>
+			)
+		}
 
-    return digits;
-  }
-// Для =
-  const calculate = () => {
-    setCalc(eval(calc).toString());
-  }
-// Для удаления всего
-  const deleteAll = () =>{
-    setResult('');
-    setCalc('');
-   }
-// Для Удаления части
- const deleteLast = () => {
-  if (calc == '') {
-    return;
-  }
+		return digits;
+	}
+	
+	// Для получения результата "="
+	const calculate = () => {
+		setCalc(eval(calc).toString());
+	}
+	
+	// Для удаления всего
+	const deleteAll = () => {
+		setResult('');
+		setCalc('');
+	}
+	
+	// Для удаления части
+	const deleteLast = () => {
+		if (calc == '') {
+			return;
+		}
 
-  const value = calc.slice(0, -1);
+		const value = calc.slice(0, -1);
 
-  setCalc(value);
- }
+		setCalc(value);
+	}
 
-  return (
-    <div className="App">
-      <div className="calculator">
-        {/* <div className="default">
-          <button onClick={deleteAll}>C</button>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-         </div> */}
-        <div className="display">
-          {result ? <span>({result})</span> : '' } { calc || "0"}
-          {/* <button onClick={deleteAll}>C</button> */}
-        </div>
-        {/* Создаём кнопки простейших математических операций */}
-        <div className="operators">
-          <button onClick={() => updateCalc('/')}>/</button>
-          <button onClick={() => updateCalc('*')}>*</button>
-          <button onClick={() => updateCalc('+')}>+</button>
-          <button onClick={() => updateCalc('-')}>-</button>
+	return (
+		<div className="App">
+			<div className="calculator">
+
+				<div className="display">
+					{result ? <span>({result})</span> : '' } { calc || "0"}
+				</div>
+        
+				{/* Создание кнопок математических операций */}
+				<div className="operators">
+					<button onClick={() => updateCalc('/')}>/</button>
+					<button onClick={() => updateCalc('*')}>*</button>
+					<button onClick={() => updateCalc('+')}>+</button>
+					<button onClick={() => updateCalc('-')}>-</button>
           
-          <button onClick={deleteLast}>DEL</button>
-          <button onClick={deleteAll}>C</button>
-        </div>
-        {/* Создаём кнопки 0, точку и равно, а так же испоьзуем функцию создания оставшихся цифр  */}
-        <div className="digits">
-          { createDigits() }
-          <button onClick={() => updateCalc('0')}>0</button>
-          <button onClick={() => updateCalc('.')}>.</button>
-
-          <button onClick={calculate}>=</button>
-        </div>
-
-      </div>
-    </div>
-  );
+					<button onClick={deleteLast}>DEL</button>
+					<button onClick={deleteAll}>C</button>
+				</div>
+        
+				{/* Создание кнопки "0", "точки", "=", 
+					а также остальных цифр */ }
+				<div className="digits">
+					{ createDigits() }
+					<button onClick={() => updateCalc('0')}>0</button>
+					<button onClick={() => updateCalc('.')}>.</button>
+					<button onClick={calculate}>=</button>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
